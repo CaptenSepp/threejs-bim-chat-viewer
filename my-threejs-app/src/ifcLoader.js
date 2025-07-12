1a79cd52298d2493db1dec40fd38bc1b4fef2321
Capten SEPP <53124407+CaptenSepp@users.noreply.github.com>
2025-07-12 12:45:38 +0200

    Add ifcLoader.js to handle loading IFC models using OBC components

---
+++ my-threejs-app/src/ifcLoader.js
@@ -0,0 +1,36 @@
+import * as OBC from '@thatopen/components';
+import * as THREE from "three";
+
+export async function loadIfcModel(scene, camera,
+  url = 'https://thatopen.github.io/engine_components/resources/ifc/small.ifc') {
+
+  const components = new OBC.Components();
+  components.scene = scene;
+  components.camera = camera;
+  components.init();
+
+  const ifcLoader = components.get(OBC.IfcLoader);
+  await ifcLoader.setup();
+
+  const buffer = await (await fetch(url)).arrayBuffer();
+  const model = await ifcLoader.load(new Uint8Array(buffer));
+
+  model.traverse(o => {
+    if (o.isMesh && !o.name) o.name = `IFC-${o.userData.expressID}`;
+  });
+  // scene.add(model);
+
+  // const cameraUtils = components.get(OBC.CameraUtils);
+  // cameraUtils.fitToSelection(model);
+
+  /* Kamera grob einpassen ------------------------- */
+  const box = new THREE.Box3().setFromObject(model);
+  const size = box.getSize(new THREE.Vector3());
+  const center = box.getCenter(new THREE.Vector3());
+
+  camera.position.set(center.x,
+    center.y + size.y,
+    center.z + size.z * 2);
+  camera.lookAt(center);
+
+}
